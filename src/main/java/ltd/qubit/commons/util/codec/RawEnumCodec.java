3b4efd7d995514e0edc402fdceb45f46d0c2c4de
Haixing Hu <starfish.hu@gmail.com>
2025-02-06 22:51:03 +0800

    feat: rowEnumCodec provide an option to support @JsonValue annotation

--- src/main/java/ltd/qubit/commons/util/codec/RawEnumCodec.java
+++ src/main/java/ltd/qubit/commons/util/codec/RawEnumCodec.java
@@ -8,27 +8,108 @@
 ////////////////////////////////////////////////////////////////////////////////
 package ltd.qubit.commons.util.codec;
 
+import java.lang.reflect.InvocationTargetException;
+import java.lang.reflect.Method;
+
+import javax.annotation.Nullable;
+import javax.annotation.concurrent.Immutable;
+
+import com.fasterxml.jackson.annotation.JsonValue;
+
 import ltd.qubit.commons.lang.ArrayUtils;
 import ltd.qubit.commons.text.Stripper;
 
 import static ltd.qubit.commons.lang.Argument.requireNonNull;
 import static ltd.qubit.commons.lang.StringUtils.isEmpty;
 
+/**
+ * The codec of the enumeration class.
+ *
+ * @author Haixing Hu
+ */
 @SuppressWarnings("rawtypes")
+@Immutable
 public class RawEnumCodec implements Codec<Enum, String> {
 
   private final Class<? extends Enum> enumClass;
 
+  private final boolean supportJsonValue;
+
+  @Nullable
+  private final Method valueGetter;
+
+  /**
+   * Constructs a {@link RawEnumCodec} object.
+   */
   public RawEnumCodec() {
-    this.enumClass = Enum.class;
+    this(Enum.class, false);
   }
 
+  /**
+   * Constructs a {@link RawEnumCodec} object.
+   *
+   * @param supportJsonValue
+   *     whether to support the {@link JsonValue} annotation.
+   */
+  public RawEnumCodec(final boolean supportJsonValue) {
+    this(Enum.class, supportJsonValue);
+  }
+
+  /**
+   * Constructs a {@link RawEnumCodec} object.
+   *
+   * @param enumClass
+   *     the class object of the enumeration class.
+   */
   public RawEnumCodec(final Class<? extends Enum> enumClass) {
-    this.enumClass = requireNonNull("enumClass", enumClass);
+    this(enumClass, false);
   }
 
+  /**
+   * Constructs a {@link RawEnumCodec} object.
+   *
+   * @param enumClass
+   *     the class object of the enumeration class.
+   * @param supportJsonValue
+   *     whether to support the {@link JsonValue} annotation.
+   */
+  public RawEnumCodec(final Class<? extends Enum> enumClass, final boolean supportJsonValue) {
+    this.enumClass = requireNonNull("enumClass", enumClass);
+    this.supportJsonValue = supportJsonValue;
+    if (supportJsonValue) {
+      this.valueGetter = getValueGetter(enumClass);
+    } else {
+      this.valueGetter = null;
+    }
+  }
+
+  private static Method getValueGetter(final Class<? extends Enum> enumClass) {
+    for (final Method method : enumClass.getDeclaredMethods()) {
+      if ((method.getParameterCount() == 0) && method.isAnnotationPresent(JsonValue.class)) {
+        return method;
+      }
+    }
+    return null;
+  }
+
+  /**
+   * Gets the class object of the enumeration class.
+   *
+   * @return
+   *     the class object of the enumeration class.
+   */
   public final Class<? extends Enum> getEnumClass() {
     return enumClass;
+  }
+
+  /**
+   * Gets whether to support the {@link JsonValue} annotation.
+   *
+   * @return
+   *     whether to support the {@link JsonValue} annotation.
+   */
+  public boolean isSupportJsonValue() {
+    return supportJsonValue;
   }
 
   @Override
@@ -37,22 +118,59 @@ public class RawEnumCodec implements Codec<Enum, String> {
     if (isEmpty(str)) {
       return null;
     }
-    // normalize the enum names
-    final String name = str.toUpperCase().replace('-', '_');
-    final Enum[] enumValues = enumClass.getEnumConstants();
-    for (final Enum e : enumValues) {
-      if (e.name().toUpperCase().equals(name)) {
-        return e;
-      }
+    final Enum result = (valueGetter == null ? decodeWithName(str) : decodeWithValue(str));
+    if (result != null) {
+      return result;
     }
     final String message = String.format("Cannot deserialize value of type `%s`"
       + " from string \"%s\": not one of the values accepted for Enum class: %s",
-        enumClass.getName(), source, ArrayUtils.toString(enumValues));
+        enumClass.getName(), source, ArrayUtils.toString(enumClass.getEnumConstants()));
     throw new DecodingException(message);
   }
 
+  @Nullable
+  private Enum decodeWithName(final String name) {
+    // normalize the enum names
+    final String normalizedName = name.toUpperCase().replace('-', '_');
+    final Enum[] values = enumClass.getEnumConstants();
+    for (final Enum e : values) {
+      if (e.name().toUpperCase().equals(normalizedName)) {
+        return e;
+      }
+    }
+    return null;
+  }
+
+  private Enum decodeWithValue(final String value) throws DecodingException {
+    assert valueGetter != null;
+    final Enum[] values = enumClass.getEnumConstants();
+    for (final Enum e : values) {
+      final Object v;
+      try {
+        v = valueGetter.invoke(e);
+      } catch (final IllegalAccessException | InvocationTargetException ex) {
+        throw new DecodingException(ex);
+      }
+      if (v.equals(value)) {
+        return e;
+      }
+    }
+    return null;
+  }
+
   @Override
-  public String encode(final Enum source) {
-    return (source == null ? null : source.name());
+  public String encode(final Enum source) throws EncodingException {
+    if (source == null) {
+      return null;
+    } else if (valueGetter != null) {
+      try {
+        final Object value = valueGetter.invoke(source);
+        return (value == null ? null : value.toString());
+      } catch (final IllegalAccessException | InvocationTargetException e) {
+        throw new EncodingException(e);
+      }
+    } else {
+      return source.name();
+    }
   }
 }
---
+++ src/test/java/ltd/qubit/commons/util/codec/RawEnumCodecTest.java
@@ -0,0 +1,87 @@
+/// /////////////////////////////////////////////////////////////////////////////
+//
+//    Copyright (c) 2024 - 2025.
+//    Nanjing Xinglin Digital Technology Co., Ltd.
+//
+//    All rights reserved.
+//
+////////////////////////////////////////////////////////////////////////////////
+package ltd.qubit.commons.util.codec;
+
+import org.junit.jupiter.api.Test;
+
+import com.fasterxml.jackson.annotation.JsonValue;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.junit.jupiter.api.Assertions.assertNull;
+import static org.junit.jupiter.api.Assertions.assertThrows;
+
+public class RawEnumCodecTest {
+
+  enum TestEnum {
+    VALUE_1("x1"),
+    VALUE_2("x2"),
+    VALUE_3("x3");
+
+    private final String value;
+
+    TestEnum(final String value) {
+      this.value = value;
+    }
+
+    @JsonValue
+    public String getValue() {
+      return value;
+    }
+  }
+
+  @Test
+  void encodeEnumWithJsonValue() throws EncodingException {
+    final RawEnumCodec codec = new RawEnumCodec(TestEnum.class, true);
+    assertEquals("x1", codec.encode(TestEnum.VALUE_1));
+  }
+
+  @Test
+  void encodeEnumWithoutJsonValue() throws EncodingException {
+    final RawEnumCodec codec = new RawEnumCodec(TestEnum.class, false);
+    assertEquals("VALUE_1", codec.encode(TestEnum.VALUE_1));
+  }
+
+  @Test
+  void decodeEnumWithJsonValue() throws DecodingException {
+    final RawEnumCodec codec = new RawEnumCodec(TestEnum.class, true);
+    assertEquals(TestEnum.VALUE_1, codec.decode("x1"));
+    assertThrows(DecodingException.class, () -> codec.decode("VALUE_1"));
+  }
+
+  @Test
+  void decodeEnumWithoutJsonValue() throws DecodingException {
+    final RawEnumCodec codec = new RawEnumCodec(TestEnum.class, false);
+    assertEquals(TestEnum.VALUE_1, codec.decode("VALUE_1"));
+    assertThrows(DecodingException.class, () -> codec.decode("x1"));
+  }
+
+  @Test
+  void decodeEnumWithInvalidValue() {
+    final RawEnumCodec codec = new RawEnumCodec(TestEnum.class, true);
+    assertThrows(DecodingException.class, () -> codec.decode("invalid_value"));
+  }
+
+  @Test
+  void decodeEnumWithEmptyString() throws DecodingException {
+    final RawEnumCodec codec = new RawEnumCodec(TestEnum.class, false);
+    assertNull(codec.decode(""));
+  }
+
+  @Test
+  void decodeEnumWithNull() throws DecodingException {
+    final RawEnumCodec codec = new RawEnumCodec(TestEnum.class, false);
+    assertNull(codec.decode(null));
+  }
+
+  @Test
+  void encodeEnumWithNull() throws EncodingException {
+    final RawEnumCodec codec = new RawEnumCodec(TestEnum.class, false);
+    assertNull(codec.encode(null));
+  }
+}
