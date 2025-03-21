f631766a9235bf67dbefea0b252183bbc183aa7f
Haixing Hu <starfish.hu@gmail.com>
2025-03-21 14:28:16 +0800

    refactor: refactor

--- src/test/java/ltd/qubit/commons/testbed/model/Order.java
+++ src/test/java/ltd/qubit/commons/testbed/model/Order.java
@@ -8,30 +8,33 @@
 ////////////////////////////////////////////////////////////////////////////////
 package ltd.qubit.commons.testbed.model;
 
-import java.io.Serial;
-import java.io.Serializable;
 import java.math.BigDecimal;
+
+import ltd.qubit.commons.lang.Equality;
+import ltd.qubit.commons.lang.Hash;
+import ltd.qubit.commons.text.tostring.ToStringBuilder;
 
 /**
  * A test model class representing an order.
  *
  * @author Haixing Hu
  */
-public class Order implements Serializable {
-
-  @Serial
-  private static final long serialVersionUID = 1L;
+public class Order {
 
   private Customer customer;
   private String status;
   private BigDecimal amount;
   private Info info;
 
+  public Order() {
+    // empty
+  }
+
   public Customer getCustomer() {
     return customer;
   }
 
-  public void setCustomer(Customer customer) {
+  public void setCustomer(final Customer customer) {
     this.customer = customer;
   }
 
@@ -39,7 +42,7 @@ public class Order implements Serializable {
     return status;
   }
 
-  public void setStatus(String status) {
+  public void setStatus(final String status) {
     this.status = status;
   }
 
@@ -47,7 +50,7 @@ public class Order implements Serializable {
     return amount;
   }
 
-  public void setAmount(BigDecimal amount) {
+  public void setAmount(final BigDecimal amount) {
     this.amount = amount;
   }
 
@@ -55,7 +58,41 @@ public class Order implements Serializable {
     return info;
   }
 
-  public void setInfo(Info info) {
+  public void setInfo(final Info info) {
     this.info = info;
+  }
+
+  @Override
+  public boolean equals(final Object o) {
+    if (this == o) {
+      return true;
+    }
+    if ((o == null) || (getClass() != o.getClass())) {
+      return false;
+    }
+    final Order other = (Order) o;
+    return Equality.equals(customer, other.customer) && Equality.equals(status, other.status) && Equality.equals(amount,
+        other.amount) && Equality.equals(info, other.info);
+  }
+
+  @Override
+  public int hashCode() {
+    final int multiplier = 7;
+    int result = 3;
+    result = Hash.combine(result, multiplier, customer);
+    result = Hash.combine(result, multiplier, status);
+    result = Hash.combine(result, multiplier, amount);
+    result = Hash.combine(result, multiplier, info);
+    return result;
+  }
+
+  @Override
+  public String toString() {
+    return new ToStringBuilder(this)
+        .append(Order::getCustomer, customer)
+        .append(Order::getStatus, status)
+        .append(Order::getAmount, amount)
+        .append(Order::getInfo, info)
+        .toString();
   }
 }
